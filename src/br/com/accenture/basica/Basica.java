fed474af572eceab08b0538c75d0afd93f37d0c1
Yelken Gonzales <yelkezin@gmail.com>
2016-05-26 15:07:58 -0300

    Alterações nas entidades Básica e Escola

--- src/br/com/accenture/basica/Basica.java
+++ src/br/com/accenture/basica/Basica.java
@@ -1,7 +1,17 @@
 package br.com.accenture.basica;
 
+import javax.persistence.Column;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.MappedSuperclass;
+
+@MappedSuperclass
 public class Basica {
 
+    @Id
+    @GeneratedValue(strategy=GenerationType.AUTO)
+    @Column(name = "id")
     private Integer identifier;
 
     public Integer getIdentifier() {
--- src/br/com/accenture/escola/Escola.java
+++ src/br/com/accenture/escola/Escola.java
@@ -1,8 +1,39 @@
 package br.com.accenture.escola;
 
+import javax.persistence.AttributeOverride;
+import javax.persistence.AttributeOverrides;
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.Inheritance;
+import javax.persistence.InheritanceType;
+import javax.persistence.Table;
+
+
+@Entity
+@Table(name="escola")
+@Inheritance(strategy=InheritanceType.TABLE_PER_CLASS)  
+@AttributeOverrides(value={  
+        @AttributeOverride(name="id",column=@Column(name="id")),  
+})  
 public class Escola {
-    
+
     private String descricao;
     private String endereco;
-   
+
+    public String getDescricao() {
+        return descricao;
+    }
+
+    public void setDescricao(String descricao) {
+        this.descricao = descricao;
+    }
+
+    public String getEndereco() {
+        return endereco;
+    }
+
+    public void setEndereco(String endereco) {
+        this.endereco = endereco;
+    }
+
 }
