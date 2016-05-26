217f86b33bba2051ab36411d1213e2466f1f3712
Yelken Gonzales <yelkezin@gmail.com>
2016-05-26 15:27:29 -0300

    DAO

--- src/br/com/accenture/inscricao/InscricaoDAO.java
+++ src/br/com/accenture/inscricao/InscricaoDAO.java
@@ -1,5 +1,28 @@
 package br.com.accenture.inscricao;
 
-public class InscricaoDAO {
+import org.hibernate.Session;
+import org.hibernate.Transaction;
 
+import br.com.accenture.conexao.ConexaoHibernate;
+
+public class InscricaoDAO {
+    
+    private Session session;
+    
+    public void salvar(Inscricao i) {
+        session = ConexaoHibernate.getInstance();
+        Transaction tx = null;
+
+        try {
+                tx = session.beginTransaction();
+                session.save(i);
+                tx.commit();
+        } catch (Exception e) {
+                e.printStackTrace();
+                tx.rollback();
+        } finally {
+                session.close();
+        }
+}
+   
 }
